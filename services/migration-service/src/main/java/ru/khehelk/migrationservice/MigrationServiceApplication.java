e834f79a7288b5e294f77d4304283299cac10197
danil.markov <renue.markov@crpt.ru>
2024-11-04 14:23:51 +0400

    Initial commit

---
+++ services/migration-service/src/main/java/ru/khehelk/migrationservice/MigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.khehelk.migrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrationServiceApplication.class, args);
+    }
+
+}
---
+++ services/routes-service/src/test/java/ru/khehelk/routesservice/RoutesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.khehelk.routesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
