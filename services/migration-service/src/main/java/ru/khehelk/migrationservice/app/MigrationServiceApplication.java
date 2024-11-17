c10e75dcca8f383fbda417883921b3772e2655c6
danil.markov <renue.markov@crpt.ru>
2024-11-17 17:54:36 +0400

    MigrationServiceApplication перенесен в поддиректорию app

--- services/migration-service/src/main/java/ru/khehelk/migrationservice/app/MigrationServiceApplication.java
+++ services/migration-service/src/main/java/ru/khehelk/migrationservice/app/MigrationServiceApplication.java
@@ -1,4 +1,4 @@
-package ru.khehelk.migrationservice;
+package ru.khehelk.migrationservice.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
